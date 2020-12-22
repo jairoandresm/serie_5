d31b280239b63477ae411d4114b26bbcea4ca9df
jairoandresm <jaanmorales@utp.edu.co>
2020-12-21 23:16:19 -0500

    Seleccionador con if

--- serie5.py
+++ serie5.py
@@ -3,7 +3,7 @@ con la idea de anexar mas adelante la interfaz gráfica del transformador'''
 
 import json
 
-#trafo_objetivo = str(input('escriba el valor del transformador : '))
+trafo_objetivo = str(input('escriba el valor del transformador : '))
 
 '''El comando with open abre el json como una lista de diccionarios, de esta manera
 para acceder a un dato específico se debe usar [0]['KVA'] el cero para la lista y el KVA para la llave del diccionario'''
@@ -11,4 +11,10 @@ para acceder a un dato específico se debe usar [0]['KVA'] el cero para la lista
 with open('data/TRAFOS.json') as contenido:
     lista_de_datos = json.loads(contenido.read())
     
-print(lista_de_datos[0]['KVA'])
+
+if trafo_objetivo == '15':
+    print(lista_de_datos[0]['KVA'])
+elif trafo_objetivo == '30':
+    print(lista_de_datos[1])
+else:
+    print('no coincide')
