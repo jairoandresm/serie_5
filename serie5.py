ec68d26dbb4ad12930da48c37af07b93ccbca3f4
jairoandresm <jaanmorales@utp.edu.co>
2020-12-21 21:41:06 -0500

    primer commit sobre el manejo de json

---
+++ serie5.py
@@ -0,0 +1,14 @@
+'''la idea del programa es seleccionar una capacidad del transformador y que el algoritmo le muestre los datos del serie 5
+con la idea de anexar mas adelante la interfaz gráfica del transformador'''
+
+import json
+
+#trafo_objetivo = str(input('escriba el valor del transformador : '))
+
+'''El comando with open abre el json como una lista de diccionarios, de esta manera
+para acceder a un dato específico se debe usar [0]['KVA'] el cero para la lista y el KVA para la llave del diccionario'''
+
+with open('data/TRAFOS.json') as contenido:
+    lista_de_datos = json.loads(contenido.read())
+    
+print(lista_de_datos[0]['KVA'])
